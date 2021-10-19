b1209cd9ebd5c8f195671a1a37a45d9e075c81fe
AM2i9 <brennanpatrick20@gmail.com>
2021-10-19 19:16:48 -0400

    Init Project

---
+++ src/main/java/fhsmc/minecraft/gary/GaryTheGatekeeper.java
@@ -0,0 +1,14 @@
+package fhsmc.minecraft.gary;
+
+public final class GaryTheGatekeeper extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
