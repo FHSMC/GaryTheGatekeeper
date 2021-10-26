936cd7870af87b352bf4505de19207cb8befeef0
AM2i9 <brennanpatrick20@gmail.com>
2021-10-26 16:03:39 -0400

    Wow! JDA is so much easier!

--- src/main/java/fhsmc/minecraft/gary/Bot/GaryBot.java
+++ src/main/java/fhsmc/minecraft/gary/Bot/GaryBot.java
@@ -1,4 +1,21 @@
 package fhsmc.minecraft.gary.Bot;
 
+import fhsmc.minecraft.gary.GaryTheGatekeeper;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
 public class GaryBot {
+
+    private static JDA client;
+
+    public static void run() throws LoginException {
+        JDABuilder botBuilder = JDABuilder.createDefault(GaryTheGatekeeper.getConfig().getString("discord.token"));
+
+        botBuilder.setActivity(Activity.playing("around with the whitelist"));
+
+        client = botBuilder.build();
+    }
 }
