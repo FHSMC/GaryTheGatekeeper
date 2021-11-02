6b4e20e85c17b8eee4446861fe4e4cbd0a6f0576
Patrick Brennan <brennanpatrick20@gmail.com>
2021-11-02 13:17:09 -0400

    Create beginnings of GoogleOAuth system

---
+++ src/main/java/fhsmc/minecraft/gary/GoogeOAuth.java
@@ -0,0 +1,40 @@
+package fhsmc.minecraft.gary;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class GoogeOAuth {
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    private static final OkHttpClient httpClient = new OkHttpClient();
+
+    private static String get(String url) throws IOException{
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    private static String post(String url, String jsonBody) throws IOException{
+        RequestBody body = RequestBody.create(JSON, jsonBody);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static String getNewDeviceAndUserCodes() throws IOException {
+        String url = "https://oauth2.googleapis.com/device/code?client_id="
+                + Config.getString("google.client-id")
+                + "&scope=email";
+        String data = post(url, "{}");
+        return data;
+    }
+}
