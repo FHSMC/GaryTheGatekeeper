6898abfdd6336b6ad1a552a189944911173fc826
Patrick Brennan <brennanpatrick20@gmail.com>
2021-11-02 17:02:32 -0400

    Implementing better Storage methods

--- src/main/java/fhsmc/minecraft/gary/Listeners.java
+++ src/main/java/fhsmc/minecraft/gary/Listeners.java
@@ -6,14 +6,30 @@ import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
+import java.sql.SQLException;
+
 public class Listeners implements Listener {
 
     @EventHandler
-    public void onPostLogin(PostLoginEvent event) {
+    public void onPostLogin(PostLoginEvent event){
         ProxiedPlayer player = event.getPlayer();
 
-        if (!Storage.checkIfWhitelisted(player)) {
-            player.disconnect(new TextComponent("You are not whitelisted!"));
+        try {
+            if (!Storage.isPlayerUUIDWhitelisted(player.getUniqueId().toString())) {
+                if (!Storage.isPlayerIGNWhitelisted(player.getName())){
+                    player.disconnect(new TextComponent("You are not whitelisted!"));
+                } else {
+                    Storage.setUUIDFromIGN(
+                            player.getName(),
+                            player.getUniqueId().toString(),
+                            player.getName().startsWith(".")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            player.disconnect(new TextComponent("There was an error while checking the whitelist. Please contact staff."));
+            e.printStackTrace();
         }
+
     }
 }
