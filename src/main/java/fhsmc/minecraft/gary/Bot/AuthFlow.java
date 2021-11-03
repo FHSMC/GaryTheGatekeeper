cc1a4276fd1fcbd5798f006bea75fd141db2530d
Patrick Brennan <brennanpatrick20@gmail.com>
2021-11-02 20:03:28 -0400

    Create AuthFlow process

--- src/main/java/fhsmc/minecraft/gary/Bot/AuthFlow.java
+++ src/main/java/fhsmc/minecraft/gary/Bot/AuthFlow.java
@@ -1,43 +1,104 @@
 package fhsmc.minecraft.gary.Bot;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import fhsmc.minecraft.gary.Config;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class AuthFlow {
 
+    private ButtonClickEvent _event;
+
+    private String deviceCode;
+    private String accessToken;
+
+    private Timer exec;
+
     public AuthFlow(ButtonClickEvent event) {
+        _event = event;
         JSONObject data = null;
         try {
-            data = GoogeOAuth.startAuthFlow();
+            data = GoogleOAuth.startAuthFlow();
 
-            String deviceCode = data.getString("device_code");
+            deviceCode = data.getString("device_code");
             String userCode = data.getString("user_code");
             int expires = data.getInt("expires_in");
             int interval = data.getInt("interval");
 
             String url = data.getString("verification_url");
 
-            ScheduledExecutorService exec = Executors.newScheduledThreadPool(10);
-            ScheduledFuture<?> task = exec.scheduleAtFixedRate(() -> {}, 0, interval, TimeUnit.SECONDS);
-            exec.scheduleWithFixedDelay(() -> task.cancel(true), 0, expires, TimeUnit.SECONDS);
+            exec = new Timer();
+            GoogleOAuth.pollGoogleAuth(deviceCode);
+            exec.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    pollTask();
+                }
+            }, interval * 1000, interval * 1000);
 
-            event.reply("To get whitelisted, we need to make sure you have a valid school email."
-                    + "Follow the instructions below:\n"
-                    + "*Go to "
+            exec.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    exec.cancel();
+                    exec.purge();
+                }
+            }, expires * 1000);
+
+            _event.reply("To get whitelisted, we need to make sure you have a valid school email. "
+                    + "Follow the instructions below:\n\n"
+                    + "Go to "
                     + url
                     + " and enter the following code: "
-                    + "**`" + userCode + "`**"
-                    + "\nWhen you have complete the steps there, you will be able to whitelist your Minecraft account.").setEphemeral(true).queue();
-        } catch (
-                IOException e) {
+                    + "**`" + userCode + "`**\n\n"
+                    + "When you have complete logging in there, you will be able to whitelist your Minecraft account.\n"
+                    + "*We do not store your email or anything else about your Google account. "
+                    + "This only needs to be done once to confirms you have a valid school email.*").setEphemeral(true).queue();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void pollTask() {
+        try {
+            JSONObject token_data = GoogleOAuth.pollGoogleAuth(deviceCode);
+            if (token_data.has("error") && !token_data.get("error").equals("authorization_pending")) {
+                throw new IOException("Error when polling Google servers: " + token_data.get("error"));
+            } else {
+                if (token_data.has("id_token")) {
+                    postRecieveToken(token_data.getString("id_token"));
+                    exec.cancel();
+                    exec.purge();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            _event.getHook().sendMessage("Woops, looks like we got an error from Google servers! Contact staff for help.").setEphemeral(true).queue();
+        }
+    }
+
+    private void postRecieveToken(String token) {
+        String email;
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            email = jwt.getClaim("email").toString().replace("\"", "");
+        } catch (JWTDecodeException exception){
+            exception.printStackTrace();
+            _event.getHook().sendMessage("Woops, looks like there was a problem with the data we got from Google servers. Contact staff for help.").setEphemeral(true).queue();
+            return;
+        }
+
+        if (email != null && email.endsWith(Config.getString("google.email-suffix"))) {
+            GaryBot.authFlowComplete(_event, true, email);
+        } else {
+            GaryBot.authFlowComplete(_event, false, email);
+        }
+    }
 }
--- src/main/java/fhsmc/minecraft/gary/Bot/GaryBot.java
+++ src/main/java/fhsmc/minecraft/gary/Bot/GaryBot.java
@@ -1,6 +1,7 @@
 package fhsmc.minecraft.gary.Bot;
 
 import fhsmc.minecraft.gary.Config;
+import fhsmc.minecraft.gary.Storage;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -8,6 +9,7 @@ import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.HashMap;
 
@@ -32,13 +34,40 @@ public class GaryBot extends ListenerAdapter {
     @Override
     public void onButtonClick(ButtonClickEvent event) {
         if (event.getButton().getId().equals("start_1")) {
-            if (authFlows.containsKey(event.getUser().getId())) {
-                event.reply("Hey! You've already started the process! If you dismissed the message, wait a few minutes and try again.")
-                        .setEphemeral(true)
-                        .queue();
-            } else {
-                authFlows.put(event.getUser().getId(), new AuthFlow(event));
+            try {
+                if (!Storage.discordUserInWhitelist(event.getUser().getId())){
+                    if (authFlows.containsKey(event.getUser().getId())) {
+                        event.reply("Hey! You've already started the process! If you dismissed the message, wait a few minutes and try again.")
+                                .setEphemeral(true)
+                                .queue();
+                    } else {
+                        authFlows.put(event.getUser().getId(), new AuthFlow(event));
+                    }
+                } else {
+                    whitelistProcess(event);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
+    }
+
+    public static void authFlowComplete(ButtonClickEvent event, boolean authorized, String email){
+        authFlows.remove(event.getUser().getId());
+        if (authorized) {
+            try {
+                Storage.addDiscordId(event.getUser().getId());
+            } catch (SQLException e) {
+                event.getHook().sendMessage("There was an issue storing some information. Contact staff for help.").setEphemeral(true).queue();
+                e.printStackTrace();
+            }
+            event.getHook().sendMessage("You have been authorized").setEphemeral(true).queue();
+        } else {
+            event.getHook().sendMessage("The email `" + email + "` is not a valid school email. If you believe this to be an error, please contact staff for help.").setEphemeral(true).queue();
+        }
+    }
+
+    public static void whitelistProcess(ButtonClickEvent event) {
+        event.reply("Heyyy, you're approved!").setEphemeral(true).queue();
     }
 }
