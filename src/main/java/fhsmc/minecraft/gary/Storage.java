3c1154e6919e6918293cb2d3114ab7122929f1da
Patrick Brennan <brennanpatrick20@gmail.com>
2021-12-18 14:44:51 -0500

    create update and booleanQuery methods

--- src/main/java/fhsmc/minecraft/gary/Storage.java
+++ src/main/java/fhsmc/minecraft/gary/Storage.java
@@ -9,6 +9,20 @@ public class Storage {
     private static Connection conn = null;
     private static Statement statement = null;
 
+    private static void update(String sql) throws SQLException{
+        if (conn != null && statement != null) {
+                statement.executeUpdate(sql);
+        }
+    }
+
+    private static boolean booleanQuery(String sql) throws SQLException{
+        if (conn != null && statement != null) {
+            ResultSet rs = statement.executeQuery(sql);
+            return rs.isBeforeFirst();
+        }
+        return false;
+    }
+
     public static void open(String databaseFilePath) throws SQLException, ClassNotFoundException {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -16,7 +30,7 @@ public class Storage {
             statement = conn.createStatement();
             statement.setQueryTimeout(30);
 
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS players (id INTEGER PRIMARY KEY AUTOINCREMENT, discord_id INTEGER, java_ign TEXT, java_uuid TEXT, bedrock_ign TEXT, bedrock_uuid TEXT)");
+            update("CREATE TABLE IF NOT EXISTS players (id INTEGER PRIMARY KEY AUTOINCREMENT, discord_id INTEGER, java_ign TEXT, java_uuid TEXT, bedrock_ign TEXT, bedrock_uuid TEXT)");
 
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Gary encountered a problem while trying to connect to a database: " + e.getMessage());
@@ -25,64 +39,46 @@ public class Storage {
     }
 
     public static void addDiscordId(String discord_id) throws SQLException {
-        if (conn != null && statement != null) {
-            if (!discordUserInWhitelist(discord_id)) {
-                statement.executeUpdate("INSERT INTO players (discord_id) VALUES (" + discord_id + ")");
-            }
+        if (!discordUserInWhitelist(discord_id)) {
+            update("INSERT INTO players (discord_id) VALUES (" + discord_id + ")");
         }
     }
 
     public static void setIGNFromDiscord(String discord_id, String ign, boolean bedrock) throws SQLException {
         String platform = bedrock ? "bedrock" : "java";
-        if (conn != null && statement != null) {
-            if (discordUserInWhitelist(discord_id)){
-                statement.executeUpdate("UPDATE players SET " + platform + "_ign=\"" + ign + "\" WHERE discord_id=" + discord_id);
-            } else {
-                statement.executeUpdate("INSERT INTO players (discord_id," + platform + "_ign) VALUES (" + discord_id + ",\"" + ign + "\")");
-            }
+        if (discordUserInWhitelist(discord_id)){
+            update("UPDATE players SET " + platform + "_ign=\"" + ign + "\" WHERE discord_id=" + discord_id);
+        } else {
+            update("INSERT INTO players (discord_id," + platform + "_ign) VALUES (" + discord_id + ",\"" + ign + "\")");
         }
     }
 
     public static void setUUIDFromIGN(String ign, String uuid, boolean bedrock) throws SQLException {
         String platform = bedrock ? "bedrock" : "java";
-        if (conn != null && statement != null) {
-            statement.executeUpdate("UPDATE players SET " + platform + "_uuid=\"" + uuid + "\" WHERE " + platform + "_ign=\"" + ign + "\"");
-        }
+        update("UPDATE players SET " + platform + "_uuid=\"" + uuid + "\" WHERE " + platform + "_ign=\"" + ign + "\"");
     }
 
     public static void setIGNFromUUID(String uuid, String ign, boolean bedrock) throws SQLException {
         String platform = bedrock ? "bedrock" : "java";
-        if (conn != null && statement != null) {
-            statement.executeUpdate("UPDATE players SET " + platform + "_ign=\"" + ign + "\" WHERE " + platform + "_uuid=\"" + uuid + "\"");
-        }
+        update("UPDATE players SET " + platform + "_ign=\"" + ign + "\" WHERE " + platform + "_uuid=\"" + uuid + "\"");
     }
 
     public static boolean discordUserInWhitelist(String discord_id) throws SQLException {
-        if (conn != null && statement != null) {
-            ResultSet rs = statement.executeQuery("SELECT * FROM players WHERE discord_id=" + discord_id);
-            return rs.isBeforeFirst();
-        }
-        return false;
+            return booleanQuery("SELECT * FROM players WHERE discord_id=" + discord_id);
     }
 
     public static boolean isPlayerUUIDWhitelisted(String uuid) throws SQLException {
-        if (conn != null && statement != null) {
-            return (
-                    statement.executeQuery("SELECT * FROM players WHERE java_uuid=\"" + uuid + "\"").isBeforeFirst()
-                    || statement.executeQuery("SELECT * FROM players WHERE bedrock_uuid=\"" + uuid + "\"").isBeforeFirst()
-            );
-        }
-        return false;
+        return (
+            booleanQuery("SELECT * FROM players WHERE java_uuid=\"" + uuid + "\"")
+            || booleanQuery("SELECT * FROM players WHERE bedrock_uuid=\"" + uuid + "\"")
+        );
     }
 
     public static boolean isPlayerIGNWhitelisted(String ign) throws SQLException {
-        if (conn != null && statement != null) {
-            return (
-                    statement.executeQuery("SELECT * FROM players WHERE java_ign=\"" + ign + "\"").isBeforeFirst()
-                    || statement.executeQuery("SELECT * FROM players WHERE bedrock_ign=\"" + ign + "\"").isBeforeFirst()
-            );
-        }
-        return false;
+        return (
+            booleanQuery("SELECT * FROM players WHERE java_ign=\"" + ign + "\"")
+            || booleanQuery("SELECT * FROM players WHERE bedrock_ign=\"" + ign + "\"")
+        );
     }
 
 
